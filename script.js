0fad715fbf3171dddbc98094559862e4cead4433
Harman Ark <ark.harman@gmail.com>
2021-08-07 22:20:08 -0400

    Fixed input error border

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const errorMsg = document.querySelector("#error-message");
 const successMsg = document.querySelector("#success-message");
 const errorIcon = document.querySelector("#error-icon");
 
+console.log(emailInput)
 
 submitBtn.addEventListener("click", function() {
     // Get the value of the input field with id="numb"
@@ -15,12 +16,15 @@ submitBtn.addEventListener("click", function() {
     if (inputValue.match(mailFormat)) {
         success = "Success";
         errorIcon.style.display = "none";
+        emailInput.style.border = "1px solid hsl(0, 36%, 70%)";
     } else if (inputValue === "") {
         error = "Please provide a valid email";
         errorIcon.style.display = "block";
+        emailInput.style.border = "2px solid hsl(0, 93%, 68%)";
     } else {
         error = "Please provide a valid email";
         errorIcon.style.display = "block";
+        emailInput.style.border = "2px solid hsl(0, 93%, 68%)";
     }
     
     successMsg.innerHTML = success;
